82fd238d597887b04e600a430a9d503cec1b4e3e
Hazyrzq <hafizrizqi26062004@gmail.com>
2026-06-08 11:22:09 +0700

    perbaikan bug

--- app/dashboard/admin/data/page.jsx
+++ app/dashboard/admin/data/page.jsx
@@ -400,7 +400,7 @@ export default function AdminDataPage() {
       {/* modal detail */}
       <AnimatePresence>
         {selectedHistory && (
-          <div className="fixed inset-0 z-50 flex items-center justify-center p-4 md:p-10 bg-slate-900/40 backdrop-blur-sm">
+          <div className="fixed inset-0 z-[150] flex items-center justify-center p-4 md:p-10 bg-slate-900/40 backdrop-blur-sm">
             <motion.div 
               initial={{ opacity: 0, scale: 0.95 }}
               animate={{ opacity: 1, scale: 1 }}
